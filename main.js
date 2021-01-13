9806207bf3190117bb58d8f3feddef0d0e7f0925
Liandra Monteiro <liandraammonteiro@gmail.com>
2021-01-13 13:02:16 -0300

    Juca points fixed

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ var start = function() {
     btnInscrever.addEventListener("click", function(e) {
         window.open('https://google.com');
     })
+
+    window.addEventListener("scroll", function(e) {
+        if(window.scrollY > 150){
+            document.querySelector(".navbar").style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+        }else{
+            document.querySelector(".navbar").style.backgroundColor = "#0e3aebc4";
+        }
+        console.log(window.scrollY)
+    })
 }
 
 window.addEventListener("load", start());
+
+// 530
