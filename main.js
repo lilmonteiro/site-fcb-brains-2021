b788030b883de515cfe46f0da9a0614db819d4a2
Liandra Monteiro <liandraammonteiro@gmail.com>
2021-01-14 12:16:12 -0300

    Juca considerations

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ var start = function() {
 
     window.addEventListener("scroll", function(e) {
         if (window.scrollY > 150) {
-            document.querySelector(".navbar").style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+            document.querySelector(".navbar").style.backgroundColor = "rgba(0, 0, 0, 0.85)";
         } else {
             document.querySelector(".navbar").style.backgroundColor = "#0e3aebc4";
         }
