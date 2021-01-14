6cc2a4ecd1f35885c4f80929e9b1eb999bc7207b
Liandra Monteiro <liandraammonteiro@gmail.com>
2021-01-14 11:39:45 -0300

    motion + cookie bar

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ var start = function() {
         window.open('https://matchbox.digital/estagio-fcb-brasil-2021-inscricao');
     })
 
+    cookieMethod();
+
 
     window.addEventListener("scroll", function(e) {
         if (window.scrollY > 150) {
@@ -34,12 +36,12 @@ var start = function() {
         var fullTxt = this.toRotate[i];
 
         if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
         } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
         }
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
         var that = this;
         var delta = 200 - Math.random() * 100;
@@ -47,34 +49,57 @@ var start = function() {
         if (this.isDeleting) { delta /= 2; }
 
         if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
+            delta = this.period;
+            this.isDeleting = true;
         } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
         }
 
         setTimeout(function() {
-        that.tick();
+            that.tick();
         }, delta);
     };
 
     window.onload = function() {
         var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
+        for (var i = 0; i < elements.length; i++) {
             var toRotate = elements[i].getAttribute('data-type');
             var period = elements[i].getAttribute('data-period');
             if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
+                new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-        
+
         var css = document.createElement("style");
         css.type = "text/css";
         css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
+}
+
+var cookieMethod = function() {
+    if (localStorage.getItem("Cookie") == null || localStorage.getItem("Cookie") == undefined) {
+        setTimeout(function() {
+            document.querySelector(".cookie-bar").classList.add("active");
+        }, 1000);
+
+        document.querySelector(".cookie-bar button").addEventListener("click", function(e) {
+            localStorage.setItem('Cookie', true);
+
+            setTimeout(function() {
+                document.querySelector(".cookie-bar").classList.remove("active");
+
+                setTimeout(function() {
+                    document.querySelector(".cookie-bar").style.visibility = "visible";
+                }, 100)
+
+            }, 200);
+        });
+    } else {
+        document.querySelector(".cookie-bar").style.display = "none";
+    }
 }
 
 window.addEventListener("load", start());
